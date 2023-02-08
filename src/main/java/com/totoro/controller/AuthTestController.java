16eddc4a86331756b1ebda251104540e14b91305
TotoroKingdom <634581304@qq.com>
2023-02-08 22:49:48 +0800

    feat:nginx测试

--- src/main/java/com/totoro/controller/AuthTestController.java
+++ src/main/java/com/totoro/controller/AuthTestController.java
@@ -1,15 +1,9 @@
 package com.totoro.controller;
 
-import com.totoro.auth.AuthUtil;
-import com.totoro.pojo.auth.LoginUser;
-import lombok.extern.log4j.Log4j;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 /**
  * @author:totoro
@@ -19,6 +13,11 @@ import javax.servlet.http.HttpSession;
 @Slf4j
 @RestController
 public class AuthTestController {
+
+    @GetMapping("hello")
+    public String hello(){
+        return "hello world!";
+    }
 
     @GetMapping("test1")
     @PreAuthorize("hasAuthority('test')")
