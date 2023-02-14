bb8324c1439ee0fc4c2b6e7e2b09ffa342e97f17
TotoroKingdom <634581304@qq.com>
2023-02-15 01:18:47 +0800

    feat:1-抽奖概率逻辑实现

--- src/test/java/com/totoro/GenshinApiApplicationTests.java
+++ src/test/java/com/totoro/GenshinApiApplicationTests.java
@@ -2,14 +2,12 @@ package com.totoro;
 
 import com.totoro.mapper.PrayMapper;
 import com.totoro.pojo.Card;
-import com.totoro.pojo.Pray;
 import com.totoro.service.PrayService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
-
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
@@ -26,22 +24,25 @@ class GenshinApiApplicationTests {
 
     @Test
     void contextLoads() {
-        ArrayList<Card> cards = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
+        int count = 0;
+        int i = 0;
+        ArrayList<String> cards = new ArrayList<>();
+        for (; i < 100000; i++) {
             Card card = prayService.push();
-            cards.add(card);
+            cards.add(card.getCardName());
+            String cardName = card.getCardName();
+            if (cardName.equals("三星物品")){
+                count++;
+            }
 
         }
-
         cards.forEach(System.out::println);
+        System.out.println("count=" + count);
+        System.out.println("不中奖概率=" + count/i*1.0*100 + "%");
+        System.out.println("中奖概率=" + (i-count)/i*1.0*100 + "%");
     }
 
-    @Test
-    void insertPray(){
-        Pray pray = new Pray();
-        pray.setUserId(1l);
-        prayMapper.insert(pray);
-    }
+
 
 
 }
